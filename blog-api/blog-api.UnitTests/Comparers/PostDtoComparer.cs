2994e63aef332b10fa94dd36208419bde98fda4e
Shakhzod Niyozov <shakhzod.n30@gmail.com>
2024-02-21 16:48:36 +0500

    Finish PostServiceTests

---
+++ blog-api/blog-api.UnitTests/Comparers/PostDtoComparer.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace blog_api.UnitTests;
+
+public class PostDtoComparer : IEqualityComparer<PostDto>
+{
+    public bool Equals(PostDto? x, PostDto? y)
+    {
+        if (x is null || y is null)
+            return false;
+        if (ReferenceEquals(x, y))
+            return true;
+
+        return x.Id == y.Id &&
+                x.Title == y.Title &&
+                x.Body == y.Body &&
+                x.UserFullName == y.UserFullName &&
+                x.UserId == y.UserId;
+    }
+
+    public int GetHashCode([DisallowNull] PostDto obj)
+    {
+        return obj.Id.GetHashCode();
+    }
+}
--- blog-api/blog-api.UnitTests/PostServiceTests.cs
+++ blog-api/blog-api.UnitTests/PostServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using System.Linq.Expressions;
+using AutoMapper;
 using Moq;
 
 namespace blog_api.UnitTests;
@@ -37,15 +38,11 @@ public class PostServiceTests
 
         uow.Setup(x => x.UserRepository).Returns(userRepository.Object);
         uow.Setup(x => x.PostRepository).Returns(postRepository.Object);
-
-        #endregion
-
-        #region Act
-
         var service = new PostService(uow.Object, _mapper.Object);
-        await service.CreateAsync(createPostDto);
 
         #endregion
+
+        await service.CreateAsync(createPostDto);
 
         postRepository.Verify(x => x.Create(_mapper.Object.Map<Post>(createPostDto)));
     }
@@ -60,16 +57,142 @@ public class PostServiceTests
 
         uow.Setup(x => x.UserRepository).Returns(userRepository.Object);
         uow.Setup(x => x.PostRepository).Returns(postRepository.Object);
-
-        #endregion
-
-        #region Act
-
         var service = new PostService(uow.Object, mapper);
 
         #endregion
 
         var exception = Assert.ThrowsAsync<Exception>(async () => await service.CreateAsync(new() { UserId = 1 }));
         Assert.That(exception.Message, Is.EqualTo(exceptionMessage));
+    }
+
+    [Test]
+    public async Task DeleteAsync_WithExistingPost_DeletesPassedPost()
+    {
+        var post = new Post() { Id = 1, Body = "b", Title = "T", UserId = 1 };
+
+        uow.Setup(x => x.PostRepository).Returns(postRepository.Object);
+        postRepository.Setup(x => x.GetByIdAsync(1, "", false)).ReturnsAsync(post);
+        var service = new PostService(uow.Object, mapper);
+
+        await service.DeleteAsync(post.Id);
+
+        postRepository.Verify(x => x.Delete(post));
+    }
+
+    [Test]
+    public void DeleteAsync_WithNoPosts_ThrowsException()
+    {
+        var exceptionMessage = "Post with provided Id was not found.";
+
+        uow.Setup(x => x.PostRepository).Returns(postRepository.Object);
+        postRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>(), "", false)).ReturnsAsync(() => null);
+        var service = new PostService(uow.Object, mapper);
+
+        var exception = Assert.ThrowsAsync<Exception>(async () => await service.DeleteAsync(It.IsAny<int>()));
+
+        Assert.That(exception.Message, Is.EqualTo(exceptionMessage));
+    }
+
+    [Test]
+    public async Task GetAllAsync_WhenCalled_ReturnPostDtos()
+    {
+        #region Arrange
+
+        var posts = new List<Post>()
+        {
+            new Post() { Id = 1, Body = "B", Title = "T", UserId = 1},
+            new Post() { Id = 2, Body = "B1", Title = "T1", UserId = 2}
+        };
+
+        var expected = mapper.Map<List<PostDto>>(posts);
+        uow.Setup(x => x.PostRepository).Returns(postRepository.Object);
+        postRepository.Setup(x => x.GetAllAsync(It.IsAny<Expression<Func<Post, bool>>>(), It.IsAny<string>(), It.IsAny<bool>())).ReturnsAsync(posts);
+
+        var service = new PostService(uow.Object, mapper);
+
+        #endregion
+
+        var result = await service.GetAllAsync();
+
+        Assert.IsTrue(expected.SequenceEqual(result, new PostDtoComparer()));
+    }
+
+    [Test]
+    public async Task GetPagingAsync_WhenCalled_ReturnsPostDtos()
+    {
+        #region Arrange
+
+        var posts = new List<Post>()
+        {
+            new() {Id = 1, Body = "B1", Title = "T1", UserId = 1},
+            new() {Id = 2, Body = "B2", Title = "T2", UserId = 2},
+            new() {Id = 3, Body = "B3", Title = "T3", UserId = 3},
+            new() {Id = 4, Body = "B4", Title = "T4", UserId = 4}
+        };
+
+        var expected = mapper.Map<List<PostDto>>(posts);
+
+        postRepository.Setup(x => x.GetPagedList(4, 1, true)).ReturnsAsync(posts);
+        uow.Setup(x => x.PostRepository).Returns(postRepository.Object);
+
+        var service = new PostService(uow.Object, mapper);
+
+        #endregion
+
+        var result = await service.GetPagingAsync(4, 1);
+
+        Assert.IsTrue(expected.SequenceEqual(result, new PostDtoComparer()));
+    }
+
+    [Test]
+    public async Task GetUserPostsAsync_WhenCalled_ReturnsUserPosts()
+    {
+        var user = new User() { Id = 1 };
+        var posts = new List<Post>()
+        {
+            new() {Id = 1, Body = "B1", Title = "T1", User = user, UserId = 1},
+            new() {Id = 2, Body = "B2", Title = "T2", User = user, UserId = 1}
+        };
+        var expected = mapper.Map<List<PostDto>>(posts);
+
+        postRepository.Setup(x => x.GetAllAsync(It.IsAny<Expression<Func<Post, bool>>>(), It.IsAny<string>(), It.IsAny<bool>())).ReturnsAsync(posts);
+        uow.Setup(x => x.PostRepository).Returns(postRepository.Object);
+
+        var service = new PostService(uow.Object, mapper);
+        var result = await service.GetUserPostsAsync(1);
+
+        Assert.IsTrue(expected.SequenceEqual(result, new PostDtoComparer()));
+    }
+
+    [Test]
+    public async Task UpdateAsync_WithCorrectPostId_UpdatesPost()
+    {
+        #region Arrange
+
+        var post = new Post() { Id = 1, Title = "Hello world", Body = "Hello world 1", UserId = 2 };
+        var updatePostDto = new UpdatePostDto() { Id = post.Id, Body = post.Body, Title = post.Title };
+        postRepository.Setup(x => x.GetByIdAsync(1, It.IsAny<string>(), It.IsAny<bool>())).ReturnsAsync(post);
+        uow.Setup(x => x.PostRepository).Returns(postRepository.Object);
+        var mockedMapper = new Mock<IMapper>();
+        mockedMapper.Setup(x => x.Map<UpdatePostDto>(post)).Returns(updatePostDto);
+
+        var service = new PostService(uow.Object, mockedMapper.Object);
+
+        #endregion
+
+        await service.UpdateAsync(mockedMapper.Object.Map<UpdatePostDto>(post));
+
+        mockedMapper.Verify(x => x.Map(updatePostDto, post));
+    }
+
+    [Test]
+    public void UpdateAsync_WithIncorrrectPostId_ThrowsException()
+    {
+        postRepository.Setup(x => x.GetByIdAsync(1, It.IsAny<string>(), It.IsAny<bool>())).ReturnsAsync(() => null);
+        uow.Setup(x => x.PostRepository).Returns(postRepository.Object);
+
+        var service = new PostService(uow.Object, mapper);
+
+        Assert.ThrowsAsync<Exception>(async () => await service.UpdateAsync(new()));
     }
 }
